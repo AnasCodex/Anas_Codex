4f9e89277edaf9cb068a209d4faa20a1922baab4
AnasCodex <anas.coding404@gmail.com>
2026-05-23 06:28:13 +0300

    update

--- components/ParticlesContainer.jsx
+++ components/ParticlesContainer.jsx
@@ -48,10 +48,10 @@ const ParticlesContainer = () => {
         },
         particles: {
           color: {
-            value: "#e68e2e",
+            value: "#fff",
           },
           links: {
-            color: "#f5d393",
+            color: "#3d6db5",
             distance: 150,
             enable: true,
             opacity: 0.5,
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
       colors: {
         primary: "#131424",
         secondary: "#393A47",
-        accent: "#F13024",
+        accent: "#3d6db5",
       },
       backgroundImage: {
         explosion: 'url("/bg-explosion.png")',
