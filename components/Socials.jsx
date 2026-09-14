5dfb672379d0fc847c623d0caed8fe7c8c9b5332
AnasCodex <anas.coding404@gmail.com>
2026-09-15 00:09:15 +0300

    update

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -32,7 +32,7 @@ export const socialData = [
 
 const Socials = () => {
   return (
-    <div className="hidden md:flex items-center gap-x-5 text-lg">
+    <div className=" md:flex items-center gap-x-5 text-lg">
       {socialData.map((social, i) => (
         <Link
           key={i}
